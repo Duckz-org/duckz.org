769b18c4bc53a9958ff74c445faa065c3a623087
Your Name <you@example.com>
2023-08-16 10:01:19 -0400

    fix: removed unused pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
 
 import sitemap from "@astrojs/sitemap";
 
+// config gets evaluated before .env files, so need to use Vite's loadEnv
+const { PUBLIC_BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://duckz.org',
+  site: PUBLIC_BASE_URL,
   integrations: [sitemap()]
 });
